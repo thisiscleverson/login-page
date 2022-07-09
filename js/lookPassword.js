b47b0241656dc246c6714bdeb69ed7f062661e86
cleverson <cleverson2020333@gmail.com>
2022-07-08 21:35:40 -0300

    arquivo javascript

---
+++ js/lookPassword.js
@@ -0,0 +1,19 @@
+const button_eye = document.querySelector('.eye__button')
+
+
+function clickButtonEye(){
+    let input__password = document.querySelector('.input__senha')
+
+    if(input__password.type == "password"){
+        input__password.type = "text"
+        button_eye.src = ".././imgs/eye-close.svg"
+
+    }else{
+        input__password.type = "password"
+        button_eye.src = ".././imgs/eye-open.svg"
+    }
+}
+
+
+
+button_eye.addEventListener('click', clickButtonEye)
