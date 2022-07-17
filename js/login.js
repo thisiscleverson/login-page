a144028b1d566af2335e53653452bb3c7fa621e7
cleverson <cleverson2020333@gmail.com>
2022-07-17 12:17:17 -0300

    rm replace 002

--- js/login.js
+++ js/login.js
@@ -86,7 +86,8 @@ function check_login(data){
 
             //verificar a senha
             if(myItem.password === data.password){
-                window.location = '.././pages/thanks.html' // ir para página de usuário [github pages]
+                window.location = './pages/thanks.html' // ir para página de usuário [github pages]
+                
                 //window.location = '../pages/thanks.html' // ir para página de usuário
             }else{
                 dataIsValid = false; // dados não é valido
